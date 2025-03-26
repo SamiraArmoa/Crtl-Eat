fb37523baa9d1920a9ea49eadc1d832ef635e649
oihanb <153312616+oihanb@users.noreply.github.com>
2025-03-26 12:03:51 +0100

    Estructura Admin Estructura

---
+++ Crtl+Eat/src/cabeceras/admin.h
@@ -0,0 +1,21 @@
+/*
+ * admin.h
+ *
+ *  Created on: 26 mar 2025
+ *      Author: oihan.bilbao
+ */
+
+#ifndef SRC_CABECERAS_ADMIN_H_
+#define SRC_CABECERAS_ADMIN_H_
+
+typedef struct{
+	char* email;
+	char* pssw;
+	char* nombre;
+	int telefono;
+
+}Admin;
+
+
+
+#endif /* SRC_CABECERAS_ADMIN_H_ */
